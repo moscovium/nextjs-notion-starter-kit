8867963010758ca46691b734ddcb857becc5f3e4
moscovium <jomohub@outlook.com>
2021-08-22 09:33:48 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -17,9 +17,7 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '0xCommonsense',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
