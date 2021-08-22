f8098f6731b52f00c0a7df86b3c70a78d2626491
moscovium <jomohub@outlook.com>
2021-08-22 09:16:19 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '184f04ac527e454aa804f8037021cf55',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Common Sense',
+  domain: 'https://0xliuming.notion.site/Commen-Sense-184f04ac527e454aa804f8037021cf55',
+  author: '0xliuming',
 
   // open graph metadata (optional)
   description: 'Example site description',
